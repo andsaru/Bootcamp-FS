a11756faddb0a1927ac70846b99e76a893020e02
andsaru <andsaru@hotmail.es>
2021-04-19 20:38:11 +0200

    React

---
+++ Modulo_6_React/cheatsheet/my-app/src/components/FirstComponent.js
@@ -0,0 +1,11 @@
+export default function FirstComponent() {
+    return (
+        <div>
+            <h3>Título</h3>
+            <small>16 de Agosto de 2025</small>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+        </div>
+    )
+}
+
+// export default FirstComponent; // Alternativa
